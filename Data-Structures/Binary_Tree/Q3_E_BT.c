37b671dcda22b067a85ece407f3ffb1e1e75f90e
chaeho5 <dkscogh1@gmail.com>
2025-04-16 21:35:27 +0900

    16days

--- Data-Structures/Binary_Tree/Q3_E_BT.c
+++ Data-Structures/Binary_Tree/Q3_E_BT.c
@@ -101,6 +101,17 @@ int main()
 int countOneChildNodes(BTNode *node)
 
 {
+    if (node == NULL){
+        return 0;
+    }
+
+    int count = 0;
+    if ((node->left == NULL && node->right != NULL) || 
+        (node->left != NULL && node->right == NULL)) {
+        count = 1;
+    }
+
+    return count + countOneChildNodes(node->left) + countOneChildNodes(node->right);
     /* add your code here */
 }
 
